24bdaeb91bb3d4ab737a4c7745b1164ae6061921
Hari200425 <htharan123@gmail.com>
2025-06-30 14:20:39 +0530

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDTN2ZR_IexJ9U_mVjWRoCc7ko3lwoySzI",
+    authDomain: "todo-2-afc5f.firebaseapp.com",
+    projectId: "todo-2-afc5f",
+    storageBucket: "todo-2-afc5f.appspot.com",
+    messagingSenderId: "344634117538",
+    appId: "1:344634117538:web:b7c1f39dd09e882d7136fb",
+    measurementId: "G-1KND0JL236"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
